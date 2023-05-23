0a72331063114b3e2f882027b4b13606e57c7e27
mattiagatti <gatti.mattia00@gmail.com>
2023-05-23 11:42:47 +0200

    reformat

--- task5.sql
+++ task5.sql
@@ -5,54 +5,39 @@
 -- Application context:
 -- staff_id, role_level, store_id
 
-CREATE
-OR REPLACE CONTEXT staff_ctx USING set_staff_ctx_pkg;
+CREATE OR REPLACE CONTEXT staff_ctx USING set_staff_ctx_pkg;
 /
 
-CREATE
-OR REPLACE PACKAGE set_staff_ctx_pkg IS
+CREATE OR REPLACE PACKAGE set_staff_ctx_pkg IS
     PROCEDURE set_staff;
 END;
 /
 
-CREATE
-OR REPLACE PACKAGE BODY set_staff_ctx_pkg IS
+CREATE OR REPLACE PACKAGE BODY set_staff_ctx_pkg IS
     PROCEDURE set_staff
-        IS
-        username         VARCHAR2(200);
-        staff_id
-STAFFS.staff_id% TYPE;
-        role_level
-STAFFS.role_level% TYPE;
-        store_id
-STAFFS.store_id% TYPE;
+    IS
+        username VARCHAR2(200);
+        staff_id STAFFS.staff_id% TYPE;
+        role_level STAFFS.role_level% TYPE;
+        store_id STAFFS.store_id% TYPE;
+    BEGIN
+        username := SYS_CONTEXT('USERENV', 'SESSION_USER');
+        SELECT STAFFS.staff_id, STAFFS.role_level, STAFFS.store_id
+        INTO staff_id, role_level, store_id
+        FROM ADMIN.STAFFS
+        WHERE UPPER(SUBSTR(STAFFS.email, 1, INSTR(STAFFS.email, '@') - 1)) = username;
 
-BEGIN
-        username
-:= SYS_CONTEXT('USERENV', 'SESSION_USER');
-SELECT STAFFS.staff_id, STAFFS.role_level, STAFFS.store_id
-INTO staff_id, role_level, store_id
-FROM ADMIN.STAFFS
-WHERE UPPER(SUBSTR(STAFFS.email, 1, INSTR(STAFFS.email, '@') - 1)) = username;
-
-DBMS_SESSION
-.
-SET_CONTEXT
-('staff_ctx', 'staff_id', staff_id);
-        DBMS_SESSION.SET_CONTEXT
-('staff_ctx', 'role_level', role_level);
-        DBMS_SESSION.SET_CONTEXT
-('staff_ctx', 'store_id', store_id);
-EXCEPTION
+        DBMS_SESSION.SET_CONTEXT('staff_ctx', 'staff_id', staff_id);
+        DBMS_SESSION.SET_CONTEXT('staff_ctx', 'role_level', role_level);
+        DBMS_SESSION.SET_CONTEXT('staff_ctx', 'store_id', store_id);
+    EXCEPTION
         WHEN NO_DATA_FOUND THEN NULL;
-END;
+    END;
 END;
 /
 
-CREATE
-OR REPLACE TRIGGER set_staff_ctx_trig
-    AFTER LOGON
-    ON DATABASE
+CREATE OR REPLACE TRIGGER set_staff_ctx_trig
+    AFTER LOGON ON DATABASE
 BEGIN
     set_staff_ctx_pkg.set_staff;
 EXCEPTION
@@ -65,24 +50,24 @@ END;
 
 GRANT SELECT ON STAFFS TO
     JUNIOR_SALESMAN, JUNIOR_WAREHOUSEMAN
-\
+/
 
 CREATE OR REPLACE FUNCTION staff_privacy(
     schema_var VARCHAR2,
     table_var VARCHAR2)
 RETURN VARCHAR2
 IS
-return_val VARCHAR2(400);
-role_level STAFFS.role_level% TYPE;
-store_id STAFFS.store_id% TYPE;
-BEGIN
-role_level := SYS_CONTEXT('staff_ctx', 'role_level');
-IF role_level > 2 THEN
-    return_val := '1=1';
-ELSE
-    store_id := SYS_CONTEXT('staff_ctx', 'store_id');
-    return_val := 'active=1 AND store_id=' || store_id;
-END IF;
+    return_val VARCHAR2(400);
+    role_level STAFFS.role_level% TYPE;
+    store_id STAFFS.store_id% TYPE;
+    BEGIN
+    role_level := SYS_CONTEXT('staff_ctx', 'role_level');
+    IF role_level > 2 THEN
+        return_val := '1=1';
+    ELSE
+        store_id := SYS_CONTEXT('staff_ctx', 'store_id');
+        return_val := 'active=1 AND store_id=' || store_id;
+    END IF;
 RETURN return_val;
 END staff_privacy;
 /
@@ -112,11 +97,11 @@ CREATE OR REPLACE FUNCTION only_assigned(
     table_var VARCHAR2)
 RETURN VARCHAR2
 IS
-return_val VARCHAR2(400);
-staff_id STAFFS.staff_id% TYPE;
-BEGIN
-staff_id := SYS_CONTEXT('staff_ctx', 'staff_id');
-return_val := 'ORDERS.staff_id = ' || staff_id;
+    return_val VARCHAR2(400);
+    staff_id STAFFS.staff_id% TYPE;
+    BEGIN
+        staff_id := SYS_CONTEXT('staff_ctx', 'staff_id');
+        return_val := 'ORDERS.staff_id = ' || staff_id;
 RETURN return_val;
 END only_assigned;
 /
@@ -146,17 +131,17 @@ CREATE OR REPLACE FUNCTION stocks_filter(
     table_var VARCHAR2)
 RETURN VARCHAR2
 IS
-return_val VARCHAR2(400);
-role_level STAFFS.role_level% TYPE;
-store_id STAFFS.staff_id% TYPE;
+    return_val VARCHAR2(400);
+    role_level STAFFS.role_level% TYPE;
+    store_id STAFFS.staff_id% TYPE;
 BEGIN
-role_level := SYS_CONTEXT('staff_ctx', 'role_level');
-IF role_level > 1 THEN
-    return_val := '1=1';
-ELSE
-    store_id := SYS_CONTEXT('staff_ctx', 'store_id');
-    return_val := 'store_id=' || store_id;
-END IF;
+    role_level := SYS_CONTEXT('staff_ctx', 'role_level');
+    IF role_level > 1 THEN
+        return_val := '1=1';
+    ELSE
+        store_id := SYS_CONTEXT('staff_ctx', 'store_id');
+        return_val := 'store_id=' || store_id;
+    END IF;
 RETURN return_val;
 END stocks_filter;
 /
